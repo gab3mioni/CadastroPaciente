58befd580a69e4f2c98d7d3d3fbb0fead5b0150c
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-12-02 20:29:41 -0300

    docs: Adicionando Javadoc ao Pessoa

--- domain/Pessoa.java
+++ domain/Pessoa.java
@@ -2,6 +2,10 @@ package domain;
 
 import domain.Validator.PessoaValidator;
 
+/**
+ * Representa uma pessoa, com atributos como ID, nome, login e senha.
+ * A validação dos dados é realizada através da classe {@link PessoaValidator}.
+ */
 public class Pessoa {
 
     private final int id;
@@ -9,6 +13,15 @@ public class Pessoa {
     private String login;
     private String senha;
 
+    /**
+     * Constrói um objeto Pessoa com os dados fornecidos.
+     * 
+     * @param id O ID da pessoa.
+     * @param nome O nome da pessoa.
+     * @param login O login da pessoa.
+     * @param senha A senha da pessoa.
+     * @throws IllegalArgumentException Se qualquer um dos parâmetros for inválido de acordo com a validação.
+     */
     public Pessoa(int id, String nome, String login, String senha) {
         PessoaValidator.validarNome(nome);
         PessoaValidator.validarLogin(login);
@@ -20,35 +33,72 @@ public class Pessoa {
         this.senha = senha;
     }
 
+    /**
+     * Retorna o ID da pessoa.
+     * 
+     * @return O ID da pessoa.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Retorna o nome da pessoa.
+     * 
+     * @return O nome da pessoa.
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Retorna o login da pessoa.
+     * 
+     * @return O login da pessoa.
+     */
     public String getLogin() {
         return login;
     }
 
+    /**
+     * Retorna a senha da pessoa.
+     * 
+     * @return A senha da pessoa.
+     */
     public String getSenha() {
         return senha;
     }
 
+    /**
+     * Altera o nome da pessoa, realizando validação.
+     * 
+     * @param nome O novo nome da pessoa.
+     * @throws IllegalArgumentException Se o nome for inválido de acordo com a validação.
+     */
     public void setNome(String nome) {
         PessoaValidator.validarNome(nome);
         this.nome = nome;
     }
 
+    /**
+     * Altera o login da pessoa, realizando validação.
+     * 
+     * @param login O novo login da pessoa.
+     * @throws IllegalArgumentException Se o login for inválido de acordo com a validação.
+     */
     public void setLogin(String login) {
         PessoaValidator.validarLogin(login);
         this.login = login;
     }
 
+    /**
+     * Altera a senha da pessoa, realizando validação.
+     * 
+     * @param senha A nova senha da pessoa.
+     * @throws IllegalArgumentException Se a senha for inválida de acordo com a validação.
+     */
     public void setSenha(String senha) {
         PessoaValidator.validarSenha(senha);
         this.senha = senha;
     }
-    
 }
