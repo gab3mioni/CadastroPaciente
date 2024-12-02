dab94eb6b8ce7f894541d51041a8987b22e986d3
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-12-02 20:33:39 -0300

    docs: Adicionando Javadoc ao CadastroPaciente

--- service/CadastroPaciente.java
+++ service/CadastroPaciente.java
@@ -7,43 +7,60 @@ import domain.Psicologo;
 import domain.Paciente;
 import ui.Menu;
 
+/**
+ * Classe principal responsável pelo cadastro de pacientes e psicólogos.
+ * Este serviço gerencia a criação e cadastro de psicólogos e pacientes nos
+ * repositórios
+ * e exibe o menu de login para o usuário.
+ */
 public class CadastroPaciente {
-    public static void main(String[] args) {
 
-        InterfaceRepository<Paciente> pacienteRepository = new PacienteRepository();
-        InterfaceRepository<Psicologo> psicologoRepository = new PsicologoRepository();
+        /**
+         * Método principal que inicializa os repositórios, cria objetos de
+         * {@link Psicologo}
+         * e {@link Paciente}, adiciona-os aos respectivos repositórios e exibe o menu
+         * de login.
+         */
+        public static void main(String[] args) {
 
-        Menu menu = new Menu(pacienteRepository,
-                psicologoRepository);
+                // Criação dos repositórios para pacientes e psicólogos
+                InterfaceRepository<Paciente> pacienteRepository = new PacienteRepository();
+                InterfaceRepository<Psicologo> psicologoRepository = new PsicologoRepository();
 
-        Psicologo Janaina = new Psicologo(
-                1,
-                "Janaina",
-                "janaina",
-                "senhaTeste123@",
-                "12345678",
-                "TCC");
+                // Criação e exibição do menu
+                Menu menu = new Menu(pacienteRepository, psicologoRepository);
 
-        psicologoRepository.adicionar(Janaina);
+                // Criação de um psicólogo e adição ao repositório
+                Psicologo Janaina = new Psicologo(
+                                1,
+                                "Janaina",
+                                "janaina",
+                                "senhaTeste123@",
+                                "12345678",
+                                "TCC");
 
-        Paciente Joao = new Paciente(
-                1,
-                "João",
-                "joao_login",
-                "Senha@123",
-                "12345678910",
-                "123456789",
-                "11912345678",
-                "Professor",
-                "Ensino Superior",
-                "Física",
-                "Carlos",
-                "Maria",
-                "Paciente com boa saúde.",
-                true);
+                psicologoRepository.adicionar(Janaina);
 
-        pacienteRepository.adicionar(Joao);
+                // Criação de um paciente e adição ao repositório
+                Paciente Joao = new Paciente(
+                                1,
+                                "João",
+                                "joao_login",
+                                "Senha@123",
+                                "12345678910",
+                                "123456789",
+                                "11912345678",
+                                "Professor",
+                                "Ensino Superior",
+                                "Física",
+                                "Carlos",
+                                "Maria",
+                                "Paciente com boa saúde.",
+                                true);
 
-        menu.exibirLogin();
-    }
+                pacienteRepository.adicionar(Joao);
+
+                // Exibe o menu de login
+                menu.exibirLogin();
+        }
 }
