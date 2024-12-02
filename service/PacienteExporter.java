129f700aebd7263a917be3546d82c38e5c760b75
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-12-02 20:34:14 -0300

    docs: Adicionando Javadoc ao PacienteExporter

--- service/PacienteExporter.java
+++ service/PacienteExporter.java
@@ -1,23 +1,43 @@
 package service;
 
 import domain.Paciente;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Classe responsável pela exportação de informações de pacientes para um arquivo de texto.
+ * A classe fornece um método para exportar os dados dos pacientes para um arquivo no formato
+ * texto, permitindo a visualização das informações em um arquivo físico.
+ */
 public class PacienteExporter {
 
+    /**
+     * Exporte a lista de pacientes para um arquivo de texto.
+     * 
+     * O método percorre a lista de pacientes e escreve as informações de cada um
+     * no arquivo especificado. Se a lista estiver vazia, uma mensagem indicando
+     * que não há pacientes cadastrados será escrita no arquivo. Caso contrário,
+     * os dados de cada paciente serão gravados no arquivo de texto, com informações
+     * como ID, nome, CPF, RG, telefone, trabalho, escolaridade, curso, nome dos pais,
+     * observações e status do paciente (ativo ou inativo).
+     * 
+     * @param pacientes Lista de pacientes a ser exportada para o arquivo.
+     * @param nomeArquivo Nome do arquivo de texto onde os dados serão salvos.
+     * @param diretorio Diretório onde o arquivo será salvo.
+     */
     public static void exportarPacientesParaTxt(
         List<Paciente> pacientes, 
         String nomeArquivo,
         String diretorio
         ) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(diretorio + nomeArquivo))) {
+            // Se a lista de pacientes estiver vazia, escreve uma mensagem no arquivo.
             if (pacientes.isEmpty()) {
                 writer.write("Nenhum paciente cadastrado.");
             } else {
+                // Caso contrário, escreve as informações de cada paciente.
                 for (Paciente paciente : pacientes) {
                     writer.write("ID: " + paciente.getId() + "\n");
                     writer.write("Nome: " + paciente.getNome() + "\n");
@@ -34,8 +54,10 @@ public class PacienteExporter {
                     writer.write("\n----------------------------------\n");
                 }
             }
+            // Mensagem de sucesso após a exportação
             System.out.println("Lista de pacientes exportada com sucesso para o arquivo: " + nomeArquivo);
         } catch (IOException e) {
+            // Exibe erro caso algo aconteça durante a exportação
             System.err.println("Erro ao exportar pacientes para o arquivo: " + e.getMessage());
         }
     }
