0a100d29cd4e6afdd9ba810f6c2415f17492a233
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-12-02 20:22:34 -0300

    refactor: Atualizando regras de negócio em PacienteValidator

--- domain/Validator/PacienteValidator.java
+++ domain/Validator/PacienteValidator.java
@@ -3,19 +3,19 @@ package domain.Validator;
 public class PacienteValidator {
 
     public static void validarCpf(String cpf) {
-        if (cpf == null || cpf.length() != 11) {
+        if (cpf == null || cpf.trim().isEmpty() || cpf.length() != 11) {
             throw new IllegalArgumentException("CPF deve ter 11 dígitos.");
         }
     }
 
-    public static void validarRg(String rg) {
-        if (rg == null || rg.trim().isEmpty()) {
+    public static void validarRg(String rg) { 
+        if (rg == null || rg.trim().isEmpty() || rg.length() != 10) {
             throw new IllegalArgumentException("RG deve ter 10 dígitos.");
         }
     }
 
     public static void validarTelefone(String telefone) {
-        if (telefone == null || telefone.length() != 11) {
+        if (telefone == null || telefone.trim().isEmpty() || telefone.length() != 11) {
             throw new IllegalArgumentException("Telefone inválido.");
         }
     }
@@ -25,5 +25,5 @@ public class PacienteValidator {
             throw new IllegalArgumentException("O campo não pode ser vazio.");
         }
     }
-    
+
 }
